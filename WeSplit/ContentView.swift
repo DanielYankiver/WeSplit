f10b6ec92ce22f2a45661bdd7c4c9e1cab81499d
Daniel Yankiver <dyankiver@gmail.com>
2023-12-11 15:28:23 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Daniel Yankiver on 12/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
